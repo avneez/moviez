a87d9aa027b2fcd86958ddf82b539e7f3a538e07
abhargkeshari <abhargkeshari@deqode.com>
2023-08-31 19:52:40 +0530

    Added api.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+import {fetchDataFromApi} from './utils/api'
+
 function App() {
+
+  useEffect(()=>{
+    apiTesting();
+  },[]);
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((res)=>{
+      console.log(res)
+    });};
+
   return (
     <>
-      <div className="App">App</div>
+      <div style={{color: 'white'}} className="App">App</div>
     </>
   );
 }
