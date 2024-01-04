ca8339d0e143ef55d1d6230df4330fba763de3dc
abhargkeshari <abhargkeshari@deqode.com>
2024-01-04 19:33:24 +0530

    Created Details section for showing individual detail of a movie or TV show

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch';
+import DetailsBanner from './detailsBanner/DetailsBanner';
 
 const Details = () => {
+// const {mediaType, id} = useParams();
+// const {data, loading} = useFetch(`/${mediaType}/${id}`);
+
   return (
-    <div>Details</div>
+    <div> <DetailsBanner /> </div>
   )
 }
 
